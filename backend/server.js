1be9ee7cd033871a3f9d944039a28f4996fed6e6
bhargesh44 <godhanidenis@gmail.com>
2022-09-30 16:49:30 +0530

    deploye

--- backend/server.js
+++ backend/server.js
@@ -45,16 +45,12 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-if (
-  process.env.NODE_ENV === "PRODUCTION" ||
-  process.env.NODE_ENV === "STAGING"
-) {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
 //middleware for error
 app.use(errorMiddleware);
 
