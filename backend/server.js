d53c4dd13703797ed956666966eef505565563dc
bharegsh44 <bhargeshgediya44@gmail.com>
2022-10-01 17:37:18 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,10 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 //connection database
 connectDatabase();
 
